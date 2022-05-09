c481635498397c3a7194a3142694e99f9063b21b
Salvatore Dininni <s.dininni@yahoo.com>
2022-05-09 15:54:02 +0200

    fontawesome installation

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import  {TASKS } from '../../mock-tasks';
+import { Task } from '../../Task';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
-export class TasksComponent implements OnInit {
 
+export class TasksComponent implements OnInit {
+  tasks: Task[] = TASKS;
+  
   constructor() { }
 
   ngOnInit(): void {
